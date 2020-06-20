be1d7423b8fcf6e7124c0d26cfefcf20c9402a9f
rohan <rkakar2000@gmail.com>
2020-06-20 23:31:06 +0530

    Convert requests to POST
    Co-authored-by: Sahil <smart.is.sahil.kumar@gmail.com>

--- lib/components/networking.dart
+++ lib/components/networking.dart
@@ -1,44 +1,65 @@
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-  bool firstrun;
-  String apikey;
-  String deviceid;
-  fetch() async {
-    final SharedPreferences myPrefs = await SharedPreferences.getInstance();
-    apikey = myPrefs.getString('apikey');
-    deviceid = myPrefs.getString('deviceid');
-    // print(apikey);
-    // print(deviceid);
-  }
+bool firstrun;
+String apikey;
+String deviceid;
 
-  Future getData(int pin,String state) async{
-    final String baseUrl = "https://cloud.boltiot.com/remote/${apikey}/digitalWrite?pin=${pin}&state=${state}&deviceName=${deviceid}";
-    //print(baseUrl);
-    http.Response response = await http.get(baseUrl);
-    if(response.statusCode==200){
-      String data = response.body;
-      var decodedData = jsonDecode(data);
-      var res = decodedData;
-      return res;
-    }
-    else{
-      print(response.statusCode);
-    } 
-  }
+fetch() async {
+  final SharedPreferences myPrefs = await SharedPreferences.getInstance();
+  apikey = myPrefs.getString('apikey');
+  deviceid = myPrefs.getString('deviceid');
+  // print(apikey);
+  // print(deviceid);
+}
 
-  Future getMultiData(String pins,String states) async{
-    final String baseUrl = "https://cloud.boltiot.com/remote/${apikey}/digitalMultiWrite?pins=${pins}&states=${states}&deviceName=${deviceid}";
-    //print(baseUrl);
-    http.Response response = await http.get(baseUrl);
-    if(response.statusCode==200){
-      String data = response.body;
-      var decodedData = jsonDecode(data);
-     var res = decodedData;
-      return res;
-    }
-    else{
-      print(response.statusCode);
-    } 
+Future getData(int pin, String state) async {
+  final String baseUrl =
+      "https://cloud.boltiot.com/remote/${apikey}/digitalWrite?pin=${pin}&state=${state}&deviceName=${deviceid}";
+  //print(baseUrl);
+  http.Response response = await http.post(
+    "http://192.168.0.107:3000/setPins",
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, dynamic>{"pin": pin, "state": state}),
+  );
+
+  if (response.statusCode == 200) {
+    String data = response.body;
+    var decodedData = jsonDecode(data);
+    var res = decodedData;
+    return res;
+  } else {
+    print(response.statusCode);
   }
+}
+
+Future getMultiData(String pins, String states) async {
+  final String baseUrl =
+      "https://cloud.boltiot.com/remote/${apikey}/digitalMultiWrite?pins=${pins}&states=${states}&deviceName=${deviceid}";
+  //print(baseUrl);
+  http.Response response = await http
+      .post(
+        "http://192.168.0.107:3000/setPins",
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, dynamic>{"pins": pins, "states": states}),
+      )
+      .timeout(
+        Duration(seconds: 30),
+      );
+
+  print(response.body);
+  if (response.statusCode == 200) {
+    String data = response.body;
+    var decodedData = jsonDecode(data);
+    var res = decodedData;
+    return res;
+  } else {
+    print(response.statusCode);
+  }
+}
--- lib/screens/controls.dart
+++ lib/screens/controls.dart
@@ -1,10 +1,10 @@
-import 'package:home_automation/components/bottom_button.dart';
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:home_automation/components/bottom_button.dart';
 import 'package:home_automation/components/icon_content.dart';
-import 'package:home_automation/components/reuseable_card.dart';
 import 'package:home_automation/components/networking.dart' as networking;
-import 'package:flushbar/flushbar.dart';
+import 'package:home_automation/components/reuseable_card.dart';
 
 class Controls extends StatefulWidget {
   static const String id = 'controls';
@@ -13,68 +13,84 @@ class Controls extends StatefulWidget {
 }
 
 class _ControlsState extends State<Controls> {
+  List<bool> active = [false, false, false, false, false, false, false];
 
-  List<bool> active = [false,false,false,false,false,false,false];
-
-  showError(var res){
+  showError(var res) {
     Flushbar(
       blockBackgroundInteraction: true,
-      title:"Some Error Occured!",
-      message:res['value'],
-        icon: Icon(FontAwesomeIcons.exclamationTriangle, color: Colors.red),
-      duration:Duration(seconds: 3),              
+      title: "Some Error Occured!",
+      message: res['value'],
+      icon: Icon(FontAwesomeIcons.exclamationTriangle, color: Colors.red),
+      duration: Duration(seconds: 3),
     )..show(context);
   }
 
   toggleActive(int index) async {
     var res = await networking.getData(index, active[index] ? "HIGH" : "LOW");
-      if(res['success']==1){
-        setState(() {
-        active[index] = !active[index];
-        active[5]=false;active[6]=false;
-      });
-    }
-    else{
-      showError(res);
-    }
-    
-  }
-  toggleScene(int index) async {
-    if(index == 0){
-     var res = await networking.getMultiData('1,2,3,4','HIGH,LOW,HIGH,HIGH');
-     if(res['success']==1){
-       setState(() {
-        active[1]=false;active[2]=true;active[3]=false;active[4]=false;active[5]=false;active[6]=true;
-      });
-     }
-     else{
-      showError(res);
-     } 
-    }
-    if(index == 1){
-      var res = await networking.getMultiData('1,2,3,4','HIGH,LOW,LOW,HIGH');
-      if(res['success']==1){
+    if (res['success'] == 1) {
       setState(() {
-        active[1]=false;active[2]=true;active[3]=true;active[4]=false;active[5]=true;active[6]=false;
-      }); 
-      }
-      else{
+        active[index] = !active[index];
+        active[5] = false;
+        active[6] = false;
+      });
+    } else {
       showError(res);
     }
+  }
+
+  toggleScene(int index) async {
+    try {
+      if (index == 0) {
+        var res =
+        await networking.getMultiData('1,2,3,4', 'HIGH,LOW,HIGH,HIGH');
+        print("toggleScene.getMultidata");
+        if (res['success'] == 1) {
+          setState(() {
+            active[1] = false;
+            active[2] = true;
+            active[3] = false;
+            active[4] = false;
+            active[5] = false;
+            active[6] = true;
+          });
+        } else {
+          showError(res);
+        }
+      }
+    } catch (e) {
+      print(e);
     }
-    
+    if (index == 1) {
+      var res = await networking.getMultiData('1,2,3,4', 'HIGH,LOW,LOW,HIGH');
+      if (res['success'] == 1) {
+        setState(() {
+          active[1] = false;
+          active[2] = true;
+          active[3] = true;
+          active[4] = false;
+          active[5] = true;
+          active[6] = false;
+        });
+      } else {
+        showError(res);
+      }
+    }
   }
 
   killAll() async {
-     var res = await networking.getMultiData('1,2,3,4','HIGH,HIGH,HIGH,HIGH');
-      if(res['success']==1){
-        setState(() {
-        active[1]=false;active[2]=false;active[3]=false;active[4]=false;active[5]=false;active[6]=false;
-      }); 
-    }
-    else{
+    var res = await networking.getMultiData('1,2,3,4', 'HIGH,HIGH,HIGH,HIGH');
+    if (res['success'] == 1) {
+      setState(() {
+        active[1] = false;
+        active[2] = false;
+        active[3] = false;
+        active[4] = false;
+        active[5] = false;
+        active[6] = false;
+      });
+    } else {
       showError(res);
-    }  
+    }
   }
 
   @override
@@ -82,19 +98,21 @@ class _ControlsState extends State<Controls> {
     networking.fetch();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-            child: Column(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Expanded(
                 flex: 1,
-                child: Row(children: <Widget>[
-                  Expanded(child: ListTile(
-                     leading:
-                        Icon(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: ListTile(
+                        leading: Icon(
                           Icons.device_hub,
                           color: Colors.white,
                         ),
@@ -105,78 +123,75 @@ class _ControlsState extends State<Controls> {
                             fontSize: 25.0,
                           ),
                         ),
-                    ),
-                  )
-                ],
+                      ),
+                    )
+                  ],
                 ),
               ),
               Expanded(
                 flex: 5,
                 child: Row(children: <Widget>[
                   Expanded(
-                    child: ReuseableCard(
-                    colour: active[1] ? Colors.lightBlue: Colors.blueGrey,
-                    cardChild: Icon_Data(
-                      icon: FontAwesomeIcons.solidLightbulb,
-                      text: 'Porch Lights',
-                    ),
-                    onPress: () {
-                        toggleActive(1);
-                    },
-                  )),
+                      child: ReuseableCard(
+                        colour: active[1] ? Colors.lightBlue : Colors.blueGrey,
+                        cardChild: Icon_Data(
+                          icon: FontAwesomeIcons.solidLightbulb,
+                          text: 'Porch Lights',
+                        ),
+                        onPress: () {
+                          toggleActive(1);
+                        },
+                      )),
                   Expanded(
                       child: ReuseableCard(
-                    colour: active[2] ? Colors.lightBlue: Colors.blueGrey,
-                    cardChild: Icon_Data(
-                      icon: FontAwesomeIcons.lightbulb,
-                      text: 'LED Light',
-                    ),
-                    onPress: () {
-                      //card tap action
-                      toggleActive(2);
-                    },
-                    )
-                  ),
-                  ]
-                ),
+                        colour: active[2] ? Colors.lightBlue : Colors.blueGrey,
+                        cardChild: Icon_Data(
+                          icon: FontAwesomeIcons.lightbulb,
+                          text: 'LED Light',
+                        ),
+                        onPress: () {
+                          //card tap action
+                          toggleActive(2);
+                        },
+                      )),
+                ]),
               ),
               Expanded(
-                flex:5,
+                flex: 5,
                 child: Row(children: <Widget>[
                   Expanded(
-                    child: ReuseableCard(
-                    colour: active[3] ? Colors.lightBlue: Colors.blueGrey,
-                    cardChild: Icon_Data(
-                      icon: FontAwesomeIcons.fan,
-                      text: 'Fan',
-                    ),
-                    onPress: () {
-                      //card tap action
-                      toggleActive(3);
-                    },
-                  )),
+                      child: ReuseableCard(
+                        colour: active[3] ? Colors.lightBlue : Colors.blueGrey,
+                        cardChild: Icon_Data(
+                          icon: FontAwesomeIcons.fan,
+                          text: 'Fan',
+                        ),
+                        onPress: () {
+                          //card tap action
+                          toggleActive(3);
+                        },
+                      )),
                   Expanded(
                       child: ReuseableCard(
-                    colour: active[4] ? Colors.lightBlue: Colors.blueGrey,
-                    cardChild: Icon_Data(
-                      icon: FontAwesomeIcons.lightbulb,
-                      text: 'Tubelight',
-                    ),
-                    onPress: () {
-                      //card tap action
-                      toggleActive(4);
-                    },
-                    )
-                  ),
-                  ]
-                ),
+                        colour: active[4] ? Colors.lightBlue : Colors.blueGrey,
+                        cardChild: Icon_Data(
+                          icon: FontAwesomeIcons.lightbulb,
+                          text: 'Tubelight',
+                        ),
+                        onPress: () {
+                          //card tap action
+                          toggleActive(4);
+                        },
+                      )),
+                ]),
               ),
               Expanded(
                 flex: 1,
-                child: Row(children: <Widget>[
-                  Expanded(child: ListTile(
-                     leading:
-                        Icon(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: ListTile(
+                        leading: Icon(
                           Icons.schedule,
                           color: Colors.white,
                         ),
@@ -187,54 +202,48 @@ class _ControlsState extends State<Controls> {
                             fontSize: 25.0,
                           ),
                         ),
-                    ),
-                  )
-                ],
+                      ),
+                    )
+                  ],
                 ),
               ),
               Expanded(
-                flex:5,
+                flex: 5,
                 child: Row(children: <Widget>[
                   Expanded(
-                    child: ReuseableCard(
-                    colour: active[5] ? Colors.lightBlue: Colors.blueGrey,
-                    cardChild: Icon_Data(
-                      icon: FontAwesomeIcons.film,
-                      text: 'Veranda',
-                    ),
-                    onPress: () {
-                      //card tap action
-                      toggleScene(1);
-                    },
-                  )),
+                      child: ReuseableCard(
+                        colour: active[5] ? Colors.lightBlue : Colors.blueGrey,
+                        cardChild: Icon_Data(
+                          icon: FontAwesomeIcons.film,
+                          text: 'Veranda',
+                        ),
+                        onPress: () {
+                          //card tap action
+                          toggleScene(1);
+                        },
+                      )),
                   Expanded(
                       child: ReuseableCard(
-                    colour: active[6] ? Colors.lightBlue: Colors.blueGrey,
-                    cardChild: Icon_Data(
-                      icon: FontAwesomeIcons.moon,
-                      text: 'Night Light',
-                    ),
-                    onPress: () {
-                      //card tap action
-                      toggleScene(0);
-                    },
-                    )
-                  ),
-                  ]
-                ),
+                        colour: active[6] ? Colors.lightBlue : Colors.blueGrey,
+                        cardChild: Icon_Data(
+                          icon: FontAwesomeIcons.moon,
+                          text: 'Night Light',
+                        ),
+                        onPress: () {
+                          //card tap action
+                          toggleScene(0);
+                        },
+                      )),
+                ]),
               ),
               Expanded(
-                flex:2,
-                  child: BottomButton(
-                  () {
+                flex: 2,
+                child: BottomButton(() {
                   killAll();
-                  },
-                 'Turn Off Everything!'),
+                }, 'Turn Off Everything!'),
               )
             ],
           ),
-        )
-      );
+        ));
   }
 }
-
